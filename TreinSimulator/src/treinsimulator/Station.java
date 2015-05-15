816faa57d21ceae02e01e4a8dc67941cb45604f4
Jasper De Roeck <jasper.deroeck4@gmail.com>
2015-05-15 16:26:54 +0200

    correctie bij 0 overstappen

--- TreinSimulator/src/treinsimulator/Station.java
+++ TreinSimulator/src/treinsimulator/Station.java
@@ -41,7 +41,8 @@ public class Station {
     }
 
     public Overstapdata juisteTrein(int n, Station doel) {
-        for (Lijn lijntje : lijnen) {
+        if (n != 0) {
+            for (Lijn lijntje : lijnen) {
                 int tijd = 0;
                 for (int i = 0; i < lijntje.getHaltes().length; i++) {
                     if (!this.getStadsnaam().equals(lijntje.getHaltes()[i].getStadsnaam())) {
@@ -52,7 +53,7 @@ public class Station {
                     } else {
                         for (int j = i + 1; j < lijntje.getHaltes().length; j++) {
                             Reis r = new Reis(lijntje.getHaltes()[j], doel);
-                            if ((r.bepaalAantalOverstappen() <= n - 1) || (r.bepaalAantalOverstappen()==0)) {
+                            if ((r.bepaalAantalOverstappen() <= n - 1) || (r.bepaalAantalOverstappen() == 0)) {
                                 Station overstap = lijntje.getHaltes()[j];
                                 Trein trein = lijntje.geefEersteTrein(tijd);
                                 System.out.println("en heeft de trein gevonden: " + trein);
@@ -65,15 +66,40 @@ public class Station {
                     }
                 }
             }
-        
+        } else {
+            for (Lijn lijntje : lijnen) {
+                int tijd = 0;
+                for (int i = 0; i < lijntje.getHaltes().length; i++) {
+                    if (!this.getStadsnaam().equals(lijntje.getHaltes()[i].getStadsnaam())) {
+                        if (i < lijntje.getSegmenten().length) {
+                            tijd += lijntje.getSegmenten()[i].tijd;
+                            tijd += lijntje.getHaltes()[i].overstaptijd;
+                        }
+                    } else {
+                        for (int j = i + 1; j < lijntje.getHaltes().length; j++) {
+                            if (doel.stadsnaam.equals(lijntje.getHaltes()[j].getStadsnaam())) {
+                                Station overstap = lijntje.getHaltes()[j];
+                                Trein trein = lijntje.geefEersteTrein(tijd);
+                                System.out.println("en heeft de trein gevonden: " + trein);
+                                Overstapdata dat = new Overstapdata(overstap, trein);
+                                return dat;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
         return null;
     }
+
     @Override
     public String toString() {
         return stadsnaam;
     }
 
-    void setKruising(Kruising kr) {
+    void setKruising(Kruising kr
+    ) {
         kruising = kr;
     }
 
